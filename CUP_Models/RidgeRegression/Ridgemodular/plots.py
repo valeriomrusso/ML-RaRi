31f5a0c4093744fe41a3db00f9550223ad640216
mik9901 <97904508+mik9901@users.noreply.github.com>
2025-01-25 19:24:39 +0100

    Sistemata plots.py

--- CUP_Models/RidgeRegression/Ridgemodular/plots.py
+++ CUP_Models/RidgeRegression/Ridgemodular/plots.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score, mean_squared_error
 def plot_training_history_CUP(history, window_size=5):
     """Visualizza l'andamento del training."""
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 12))
+    fig, (ax1) = plt.subplots(1, 1, figsize=(12, 5))
     
     # Plot delle perdite grezze
     ax1.plot(history.history['loss'], label='Training Loss')
@@ -13,17 +13,6 @@ def plot_training_history_CUP(history, window_size=5):
     ax1.set_ylabel('Loss')
     ax1.legend()
     ax1.grid()
-    
-    # Plot delle perdite smussate
-    smoothed_train = pd.Series(history.history['loss']).rolling(window=window_size).mean()
-    smoothed_val = pd.Series(history.history['val_loss']).rolling(window=window_size).mean()
-    ax2.plot(smoothed_train, 'r-', label=f'Smoothed Training Loss (window={window_size})')
-    ax2.plot(smoothed_val, 'b-', label=f'Smoothed Validation Loss (window={window_size})')
-    ax2.set_title('Smoothed Training and Validation Loss')
-    ax2.set_xlabel('Epoch')
-    ax2.set_ylabel('Loss')
-    ax2.legend()
-    ax2.grid()
     
     plt.tight_layout()
     plt.savefig('training_history.png')
