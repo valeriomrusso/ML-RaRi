2d7c19f1b7c8cde9827179eda1d724416c938d65
valeriomrusso <vale.m.rus@gmail.com>
2025-01-25 17:03:00 +0100

    modular rbf for cup

---
+++ CUP_Models/RBF/RBF modulare/main.py
@@ -0,0 +1,12 @@
+from load_data import load_and_preprocess_data_CUP
+from plots import plot_training_history_CUP
+from cross_validation_rbf import CV
+
+def main():
+    filepath = 'ML-CUP24-TR.csv'
+    X_train, X_test, Y_train, Y_test, _, _ = load_and_preprocess_data_CUP(filepath)
+    final_history, final_model = CV(X_train, X_test, Y_train, Y_test)
+    plot_training_history_CUP(final_history)
+
+if __name__ == "__main__":
+    main()
