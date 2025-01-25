23b3f99ded0e9d1170f351b98ad7cadf0fda2e84
mik9901 <97904508+mik9901@users.noreply.github.com>
2025-01-25 19:04:58 +0100

    Aggiunta Ridge Modulare con CV

---
+++ CUP_Models/RidgeRegression/Ridgemodular/model_builder.py
@@ -0,0 +1,10 @@
+from tensorflow import keras
+
+def build_model(hp):
+  model = keras.Sequential()
+  batch_size=hp.Int('batch_size', min_value = 1, max_value = 25)
+  reg = hp.Float('regularizer', min_value=1e-6, max_value=1, sampling="log")
+  model.add(keras.layers.Input(shape = (12,)))
+  model.add(keras.layers.Dense(3, kernel_regularizer= keras.regularizers.l2(reg), activation= None))
+  model.compile(loss='mse', optimizer = keras.optimizers.SGD(hp.Float("lr", min_value=1e-6, max_value=1e-1, sampling="log")))
+  return model
